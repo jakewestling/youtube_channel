0b9f7b0ce45e35b2097d41d3f800ae4aacc947cc
Jake Westling <jakewestling@gmail.com>
2020-10-15 16:52:28 -0400

    modal channel name

--- by_id.php
+++ by_id.php
@@ -11,11 +11,11 @@
     <div class="col-12 d-block text-center">
 
       <!-- Modal for Question Mark -->
-      <div class="modal fade" id="by-id-question" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal fade" id="by-id-question" tabindex="-1" role="dialog" aria-labelledby="byIDModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">What is a YouTube Channel ID?</h5>
+              <h5 class="modal-title" id="byIDModalLabel">What is a YouTube Channel ID?</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
--- search_channels.php
+++ search_channels.php
@@ -8,11 +8,28 @@
     </div>
   </div>
   <div id="search-content" class="mt-1">
+  <!-- Modal for Question Mark -->
+      <div class="modal fade" id="search-question" tabindex="-1" role="dialog" aria-labelledby="searchModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="searchModalLabel">Search for YouTube Channels</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              Please type in the name of the channel you wish to find and press search. The most relevant results are displayed first. Click on a result to see the data and information about it.
+            </div>            
+          </div>
+        </div>
+      </div>
+
     <!-- Form -->
     <div class="col-12 d-block text-center">
       <form id="search-form" class="text-center">
         <div class="input-field">
-          <input class="mr-2" type="text" placeholder="Search for channels..." id="search-input"><span class="question-help" onclick=""><i class="fa-lg fas fa-question-circle"></i></span><br>
+          <input class="mr-2" type="text" placeholder="Search for channels..." id="search-input"><span class="question-help" data-toggle="modal" data-target="#search-question"><i class="fa-lg fas fa-question-circle"></i></span><br>
           <input type="submit" value="Search" class="btn bg-red text-white btn-font border border-dark mt-1 px-4">
         </div>
       </form>
