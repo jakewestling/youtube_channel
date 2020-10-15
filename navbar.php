c45cb6f8a4411987652afcb93614f25a3798d017
Jake Westling <jakewestling@gmail.com>
2020-10-15 17:12:27 -0400

    guide not displaying

--- navbar.php
+++ navbar.php
@@ -2,10 +2,10 @@
   <div class="container-fluid">  
     <nav class="row bg-light-gray py-1 main-nav">
       <div class="col-12 px-0">        
-        <a href="#" onclick="displaySearchChannels()" class="ml-1 px-2 nav-a" id="nav-link1"><i class="fas fa-search"></i> Channels</a>
-        <a href="#" onclick="displayByID()" class="px-2 nav-a" id="nav-link2">By ID</a>
-        <a href="#" onclick="displayCompare()" class="px-2 nav-a" id="nav-link3">Compare</a>
-        <a href="#" onclick="displayGuide()" class="px-2 nav-a" id="nav-link4">Guide</a>        
+        <a href="" onclick="displaySearchChannels()" class="ml-1 px-2 nav-a" id="nav-link1"><i class="fas fa-search"></i> Channels</a>
+        <a href="" onclick="displayByID()" class="px-2 nav-a" id="nav-link2">By ID</a>
+        <a href="" onclick="displayCompare()" class="px-2 nav-a" id="nav-link3">Compare</a>
+        <a href="" onclick="displayGuide()" class="px-2 nav-a" id="nav-link4">Guide</a>        
       </div>
     </nav>
   </div>
