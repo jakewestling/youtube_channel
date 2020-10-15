43792e00274f4edfe1cc34fb2a57c541a3069b39
Jake Westling <jakewestling@gmail.com>
2020-10-15 16:20:24 -0400

    question mark modal

--- by_id.php
+++ by_id.php
@@ -9,9 +9,33 @@
   </div>
   <div id="content" class="mt-1">
     <div class="col-12 d-block text-center">
+
+      <!-- Modal for Question Mark -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">What is YouTube Channel ID?</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              A channel ID is a set of characters assigned to distinguish individual channels. For example, one ID is UClePynY267S97vwsxfbF5hg.<br><br>
+              Users: Please use the Search Channels feature. You can find channel data by searching for the channel name instead.<br><br>
+              Developers: If you already know the ID, you can find data by entering it here.
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <form id="channel-form" class="text-center">
         <div class="input-field">
-          <input class="mr-2" type="text" placeholder="Enter Channel ID..." id="channel-input"><span class="question-help" onclick=""><i class="fa-lg fas fa-question-circle"></i></span><br>
+          <input class="mr-2" type="text" placeholder="Enter Channel ID..." id="channel-input"><span class="question-help" data-toggle="modal" data-target="#exampleModal"><i class="fa-lg fas fa-question-circle"></i></span><br>
           <input type="submit" value="Get Channel Info!" class="btn bg-red text-white btn-font border border-dark mt-1 px-4">
         </div>
       </form>
