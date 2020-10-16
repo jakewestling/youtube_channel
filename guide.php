a0a553a7c5a9cb7359f6b6d28ff16953ede6b3e2
Jake Westling <jakewestling@gmail.com>
2020-10-16 18:40:00 -0400

    guide

--- guide.php
+++ guide.php
@@ -1,18 +1,16 @@
 <div class="row text-center mt-1" id="guide">  
   <div class="col-12">
-    <h1 class="display-4">How to Guide</h1>
-    <h4 class="mt-3 mb-4">YouTube Channel Info is used to find and display publicly available channel data and information.</h4>
-    
-    <h4 class="mt-3 mb-4">YouTube Channel Info is used to find and display publicly available channel data and information.</h4>
-    <h2 class="display-4">Getting Started</h2>
+    <h4>How to Guide</h4>
+    <p>YouTube Channel Info is used to find and display publicly available channel data and information.</p>
 
-    <h3 class="display-4">1. Login with Google</h3>
+    <h4>Getting Started</h4>
+    <h5>1. Login with Google</h5>
     <p>(This is required to use YouTube's database.) Click on the user icon at the top right to login. Alternatively, navigate to the <i class="fas fa-search"></i> Channels or By ID page and click login.</p>
 
-    <h3 class="display-4">2a. Search for Channels</h3>
+    <h5>2a. Search for Channels</h5>
     <p>Click the <i class="fas fa-search"></i> Channels in the navigation bar. Type in the channel name you wish to find and press search. Results display by relevance. Click a result to view the channel info.</p>
 
-    <h3 class="display-4">2b. Search By ID </h3>
+    <h5>2b. Search By ID </h5>
     <p>If you already know the YouTube channel ID (one example: UClePynY267S97vwsxfbF5hg), you can search directly using this. Navigate to the By ID module in the navigation bar. Type in the ID, and press Get Channel Info!</p>
   </div>
 </div>
