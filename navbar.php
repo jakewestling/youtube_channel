59b02600ce02492cc8023b6fe4120b3cde24acf5
Jake Westling <jakewestling@gmail.com>
2020-10-16 19:08:35 -0400

    remove compare

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
       <div class="col-12 px-0">        
         <a href="#" onclick="displaySearchChannels()" class="ml-1 px-2 nav-a" id="nav-link1"><i class="fas fa-search"></i> Channels</a>
         <a href="#" onclick="displayByID()" class="px-2 nav-a" id="nav-link2">By ID</a>
-        <a href="#" onclick="displayCompare()" class="px-2 nav-a" id="nav-link3">Compare</a>
+        <!-- <a href="#" onclick="displayCompare()" class="px-2 nav-a" id="nav-link3">Compare</a> -->
         <a href="#" onclick="displayGuide()" class="px-2 nav-a" id="nav-link4">Guide</a>        
       </div>
     </nav>
