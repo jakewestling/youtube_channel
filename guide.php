d1099d65c1279052b1231f94f845910901005b94
Jake Westling <jakewestling@gmail.com>
2020-10-16 18:58:28 -0400

    guide links

--- guide.php
+++ guide.php
@@ -5,12 +5,12 @@
 
     <h3 "mb-1">Getting Started</h3>
     <h4>1. Login with Google</h4>
-    <p>(This is required to use YouTube's database.) Click on the user icon at the top right to login. Alternatively, navigate to the <i class="fas fa-search"></i> Channels or By ID page and click login.</p>
+    <p>(This is required to use YouTube's database.) Click on the user icon at the top right to login. Alternatively, navigate to the <a href="" class="text-red" onclick="displaySearchChannels()"><i class="fas fa-search"></i> Channels</a> or <a href="" class="text-red" onclick="displayByID()">By ID</a> page and click login. You need to accept permissions so that YouTube knows who is searching through their database. You may need to click advanced the first time to accept app permissions from Alpha & Data.</p>
 
     <h4>2a. Search for Channels</h4>
     <p>Click the <i class="fas fa-search"></i> Channels in the navigation bar. Type in the channel name you wish to find and press search. Results display by relevance. Click a result to view the channel info.</p>
 
     <h4>2b. Search By ID </h4>
-    <p>If you already know the YouTube channel ID (one example: UClePynY267S97vwsxfbF5hg), you can search directly using this. Navigate to the By ID module in the navigation bar. Type in the ID, and press Get Channel Info!</p>
+    <p>If you already know the YouTube channel ID (one example: UClePynY267S97vwsxfbF5hg), you can search directly using the By ID feature. Navigate to the <a href="" class="text-red" onclick="displayByID()">By ID</a> module in the navigation bar. Type in the ID, and press Get Channel Info!</p>
   </div>
 </div>
