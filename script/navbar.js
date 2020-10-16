d890b9b9b46c061161f6bae60dfc45641ba23183
Jake Westling <jakewestling@gmail.com>
2020-10-16 19:10:28 -0400

    removed compare

--- script/navbar.js
+++ script/navbar.js
@@ -1,13 +1,13 @@
 // Navbar links
 const link1 = document.getElementById("nav-link1");
 const link2 = document.getElementById("nav-link2");
-const link3 = document.getElementById("nav-link3");
+// const link3 = document.getElementById("nav-link3");
 const link4 = document.getElementById("nav-link4");
 
 function isNonActiveLink() {
   link1.classList.remove("nav-a-active");
   link2.classList.remove("nav-a-active");
-  link3.classList.remove("nav-a-active");
+  // link3.classList.remove("nav-a-active");
   link4.classList.remove("nav-a-active");
 }
 function isActiveLink(link) {
@@ -17,14 +17,14 @@ function isActiveLink(link) {
 const home = document.querySelector('#home');
 const searchChannels = document.querySelector('#search-channels');
 const byID = document.querySelector('#by-id');
-const compare = document.querySelector('#compare');
+// const compare = document.querySelector('#compare');
 const guide = document.querySelector('#guide');
 
 function hideAllPages() {
   home.style.display = 'none';
   searchChannels.style.display = 'none';
   byID.style.display = 'none';
-  compare.style.display = 'none';
+  // compare.style.display = 'none';
   guide.style.display = 'none';
 }
 
@@ -46,12 +46,12 @@ function displayByID() {
   hideAllPages();
   byID.style.display = 'block';
 }
-function displayCompare() {
-  isNonActiveLink();
-  isActiveLink(link3);
-  hideAllPages();
-  compare.style.display = 'block';
-}
+// function displayCompare() {
+//   isNonActiveLink();
+//   isActiveLink(link3);
+//   hideAllPages();
+//   compare.style.display = 'block';
+// }
 function displayGuide() {
   isNonActiveLink();
   isActiveLink(link4);
